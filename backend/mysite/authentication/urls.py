99f5f272d972ef76f08065a0043406e3535f7f59
Oleksandr-Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2024-11-10 09:54:04 +0200

    реалізовано авторизацію/реєстрацію користувачів.

--- backend/mysite/authentication/urls.py
+++ backend/mysite/authentication/urls.py
@@ -3,9 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+from .views import RegisterUserView
 
 urlpatterns = [
+    path('register/', RegisterUserView.as_view(), name='Register-user'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
